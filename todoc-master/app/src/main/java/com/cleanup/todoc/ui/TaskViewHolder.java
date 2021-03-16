bc833fef859cad7e4942724ed27f584c1e33ee1c
JosueBek <roochard922@gmail.com>
2021-03-16 01:45:58 +0100

    Mise a jour du projjet avec Butter Kniff.

---
+++ todoc-master/app/src/main/java/com/cleanup/todoc/ui/TaskViewHolder.java
@@ -0,0 +1,96 @@
+package com.cleanup.todoc.ui;
+
+import android.annotation.SuppressLint;
+import android.content.res.ColorStateList;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cleanup.todoc.R;
+import com.cleanup.todoc.events.DeleteTaskListener;
+import com.cleanup.todoc.model.Project;
+import com.cleanup.todoc.model.Task;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class TaskViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    /**
+     * The circle icon showing the color of the project
+     */
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.img_project)
+    public AppCompatImageView imgProject;
+
+    /**
+     * The TextView displaying the name of the task
+     */
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.lbl_task_name)
+    public TextView lblTaskName;
+
+    /**
+     * The TextView displaying the name of the project
+     */
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.lbl_project_name)
+    public TextView lblProjectName;
+
+    /**
+     * The delete icon
+     */
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.img_delete)
+    public AppCompatImageView imgDelete;
+
+    /**
+     * The listener for when a task needs to be deleted
+     */
+    private DeleteTaskListener deleteTaskListener ;
+
+    /**
+     * Instantiates a new TaskViewHolder.
+     *  @param itemView the view of the task item
+     */
+    TaskViewHolder(@NonNull View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+    }
+
+    /**
+     * Binds a task to the item view.
+     *
+     * @param task the task to bind in the item view
+     * @param deleteTaskListener the listener for when a task needs to be deleted to set
+     */
+    void bind(Task task, DeleteTaskListener deleteTaskListener) {
+        this.deleteTaskListener = deleteTaskListener;
+        lblTaskName.setText(task.getName());
+        imgDelete.setOnClickListener(this);
+        imgDelete.setTag(task);
+
+        final Project taskProject = task.getProject();
+        if (taskProject != null) {
+            imgProject.setSupportImageTintList(ColorStateList.valueOf(taskProject.getColor()));
+            lblProjectName.setText(taskProject.getName());
+        } else {
+            imgProject.setVisibility(View.INVISIBLE);
+            lblProjectName.setText("");
+        }
+    }
+
+    /**
+     * override method in interface deleteTaskListener, task needs to be deleted.
+     */
+    @Override
+    public void onClick(View view) {
+        final Object tag = view.getTag();
+        if (tag instanceof Task) {
+            deleteTaskListener.onDeleteTask((Task) tag);
+        }
+    }
+}
---
+++ todoc-master/app/src/main/java/com/cleanup/todoc/ui/TasksAdapter.java
@@ -0,0 +1,58 @@
+package com.cleanup.todoc.ui;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cleanup.todoc.R;
+import com.cleanup.todoc.events.DeleteTaskListener;
+import com.cleanup.todoc.model.Task;
+
+import java.util.List;
+
+/**
+ * <p>Adapter which handles the list of tasks to display in the dedicated RecyclerView.</p>
+ */
+public class TasksAdapter extends RecyclerView.Adapter<TaskViewHolder> {
+
+    private List<Task> tasks;
+    private final DeleteTaskListener deleteTaskListener;
+
+    /**
+     * Instantiates a new TasksAdapter.
+     */
+    TasksAdapter(List<Task> items, DeleteTaskListener deleteTaskListener) {
+        this.tasks = items ;
+        this.deleteTaskListener = deleteTaskListener;
+    }
+
+    @NonNull
+    @Override
+    public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_task, viewGroup, false);
+        return new TaskViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TaskViewHolder taskViewHolder, int position) {
+        taskViewHolder.bind(tasks.get(position), deleteTaskListener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return tasks.size();
+    }
+
+    /**
+     * Updates the list of tasks the adapter deals with.
+     *
+     * @param tasks the list of tasks the adapter deals with to set
+     */
+    void updateTasks(List<Task> tasks) {
+        this.tasks = tasks;
+        notifyDataSetChanged();
+    }
+}
